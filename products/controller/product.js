1d3a48d4482dd94502377c7b68480b7e345403c9
Søren <act.wescorn@gmail.com>
2023-05-25 21:23:00 +0200

    Fixed products poopoo routes, and moved swagger doc comment to the routes they each belong  to

--- products/controller/product.js
+++ products/controller/product.js
@@ -1,15 +1,108 @@
-const db = require('../models');
+const db = require("../models");
+const Product = db.Product;
+const Op = db.Sequelize.Op;
 
-const getAll = () => new Promise((resolve, reject) => {
-    db.Product.findAll()
-    .then((products) => {
-        resolve(products);
+
+exports.create = (req, res) => {
+    Product.create({
+        name: req.body.name,
+        price: req.body.price,
+        stock: req.body.stock
     })
-    .catch((error) => {
-        reject(error);
-    });
-});
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while creating the Product."
+            });
+        });
+};
 
-module.exports = {
-    getAll
-}
+
+exports.findAll = (req, res) => {
+    const name = req.query.name;
+    var condition = name ? { name: { [Op.like]: `%${name}%` } } : null;
+    Product.findAll({ where: condition })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving products."
+            });
+        });
+};
+
+
+exports.findOne = (req, res) => {
+    const id = req.params.id;
+
+    Product.findByPk(id)
+        .then(data => {
+            if (data) {
+                res.send(data);
+            } else {
+                res.status(404).send({
+                    message: `Cannot find Product with id=${id}.`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error retrieving Product with id=" + id
+            });
+        });
+};
+
+
+exports.update = (req, res) => {
+    const id = req.params.id;
+
+    Product.update(req.body, {
+        where: { id: id }
+    })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Product was updated successfully."
+                });
+            } else {
+                res.send({
+                    message: `Cannot update Product with id=${id}. Maybe Product was not found or req.body is empty!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error updating Product with id=" + id
+            });
+        });
+};
+
+
+exports.delete = (req, res) => {
+    const id = req.params.id;
+
+    Product.destroy({
+        where: { id: id }
+    })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Product was deleted successfully!"
+                });
+            } else {
+                res.send({
+                    message: `Cannot delete Product with id=${id}. Maybe Product was not found!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Could not delete Product with id=" + id
+            });
+        });
+};
--- products/routes/productRoute.js
+++ products/routes/productRoute.js
@@ -1,150 +1,175 @@
 
 const express = require('express');
 const router = express.Router();
-const productCtrl = require('../controller/product');
+const productController = require('../controller/product');
 
-router.get('/getAllProducts', (req, res) => {
-    productCtrl.getAll()
-    .then((result) => {
-        res.send({
-            message: 'All products fetched',
-            data: result,
-        });
-    })
-    .catch((error) => {
-        res.send({
-            message: 'Some error occoured',
-            error
-        });
-    });
-});
+/**
+* @swagger
+* components:
+*   schemas:
+*     Product:
+*       type: object
+*       required:
+*         - name
+*         - price
+*         - stock
+*       properties:
+*         id:
+*           type: integer
+*           description: The auto-generated id of the product
+*         name:
+*           type: string
+*           description: The name of the product
+*         price:
+*           type: double
+*           description: The price of the product
+*         stock:
+*           type: integer
+*           description: The current stock of the product
+*       example:
+*         id: 50
+*         name: Duct tape
+*         price: 70
+*         stock: 22
+*/
+
+/**
+* @swagger
+* tags:
+*   name: Product
+*   description: The Product managing API
+* /products:
+*   get:
+*     summary: Lists all the products
+*     tags: [Products]
+*     responses:
+*       200:
+*         description: The list of the products
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 $ref: '#/components/schemas/Product'
+*/
+router.get('/', productController.findAll);
+
+/**
+* @swagger
+* tags:
+*   name: Product
+*   description: The Product managing API
+* /products/{id}:
+*   get:
+*     summary: Get the product by id
+*     tags: [Products]
+*     parameters:
+*       - in: path
+*         name: id
+*         schema:
+*           type: integer
+*         required: true
+*         description: The product id
+*     responses:
+*       200:
+*         description: The product response by id
+*         contens:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/Product'
+*       404:
+*         description: The product was not found
+*/
+router.get('/:id', productController.findOne);
+
+/**
+* @swagger
+* tags:
+*   name: Product
+*   description: The Product managing API
+* /products:
+*   post:
+*     summary: Create a new product
+*     tags: [Products]
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             $ref: '#/components/schemas/Product'
+*     responses:
+*       200:
+*         description: The created product.
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/Product'
+*       500:
+*         description: Some server error
+*/
+router.post('/', productController.create),
+
+/**
+* @swagger
+* tags:
+*   name: Product
+*   description: The Product managing API
+* /products/{id}:
+*   put:
+*    summary: Update the product by the id
+*    tags: [Products]
+*    parameters:
+*      - in: path
+*        name: id
+*        schema:
+*          type: integer
+*        required: true
+*        description: The product id
+*    requestBody:
+*      required: true
+*      content:
+*        application/json:
+*          schema:
+*            $ref: '#/components/schemas/Product'
+*    responses:
+*      200:
+*        description: The product was updated
+*        content:
+*          application/json:
+*            schema:
+*              $ref: '#/components/schemas/Product'
+*      404:
+*        description: The product was not found
+*      500:
+*        description: Some error happened
+*/
+router.put('/:id', productController.update);
+
+/**
+* @swagger
+* tags:
+*   name: Product
+*   description: The Product managing API
+* /products/{id}:
+*   delete:
+*     summary: Remove the product by id
+*     tags: [Products]
+*     parameters:
+*       - in: path
+*         name: id
+*         schema:
+*           type: integer
+*         required: true
+*         description: The product id
+*
+*     responses:
+*       200:
+*         description: The product was deleted
+*       404:
+*         description: The product was not found
+*/
+router.delete('/:id', productController.delete);
 
 module.exports = router;
 
 
-/**
- * @swagger
- * components:
- *   schemas:
- *     Product:
- *       type: object
- *       required:
- *         - name
- *         - price
- *         - stock
- *       properties:
- *         id:
- *           type: integer
- *           description: The auto-generated id of the product
- *         name:
- *           type: string
- *           description: The name of the product
- *         price:
- *           type: double
- *           description: The price of the product
- *         stock:
- *           type: integer
- *           description: The current stock of the product
- *       example:
- *         id: 50
- *         name: Duct tape
- *         price: 70
- *         stock: 22
- * tags:
- *   name: Product
- *   description: The Product managing API
- * /products:
- *   get:
- *     summary: Lists all the products
- *     tags: [Products]
- *     responses:
- *       200:
- *         description: The list of the products
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 $ref: '#/components/schemas/Product'
- *   post:
- *     summary: Create a new product
- *     tags: [Products]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/Product'
- *     responses:
- *       200:
- *         description: The created product.
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Product'
- *       500:
- *         description: Some server error
- * /products/{id}:
- *   get:
- *     summary: Get the product by id
- *     tags: [Products]
- *     parameters:
- *       - in: path
- *         name: id
- *         schema:
- *           type: integer
- *         required: true
- *         description: The product id
- *     responses:
- *       200:
- *         description: The product response by id
- *         contens:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Product'
- *       404:
- *         description: The product was not found
- *   put:
- *    summary: Update the product by the id
- *    tags: [Products]
- *    parameters:
- *      - in: path
- *        name: id
- *        schema:
- *          type: integer
- *        required: true
- *        description: The product id
- *    requestBody:
- *      required: true
- *      content:
- *        application/json:
- *          schema:
- *            $ref: '#/components/schemas/Product'
- *    responses:
- *      200:
- *        description: The product was updated
- *        content:
- *          application/json:
- *            schema:
- *              $ref: '#/components/schemas/Product'
- *      404:
- *        description: The product was not found
- *      500:
- *        description: Some error happened
- *   delete:
- *     summary: Remove the product by id
- *     tags: [Products]
- *     parameters:
- *       - in: path
- *         name: id
- *         schema:
- *           type: integer
- *         required: true
- *         description: The product id
- *
- *     responses:
- *       200:
- *         description: The product was deleted
- *       404:
- *         description: The product was not found
- */
